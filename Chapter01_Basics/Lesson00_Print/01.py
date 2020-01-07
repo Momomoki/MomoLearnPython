73a1285e043c310c2ad19365e7e844eb08dc9b3d
Peixian <55384910+Momomoki@users.noreply.github.com>
2020-01-07 14:10:27 +0800

    Added 01.py
    New file

---
+++ Chapter01_Basics/Lesson00_Print/01.py
@@ -0,0 +1,10 @@
+import time
+
+# print characters
+print('Hello, World!')
+
+# print number
+print(520)
+
+# print time with formatting
+print ('At '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+', I wrote the first line of Python code\n' )
